0f523130efa44206a3b8bec51f0c9a3e4bb26d25
Thanneer Malai Perumal <rstudio@ip-172-31-32-105.ec2.internal>
2016-11-03 22:04:28 +0000

    Added R code to build rank consensus networks (changes metanetwork repo code to randomly rank ties)

---
+++ buildConsensus2.R
@@ -0,0 +1,52 @@
+# Build rank consensus network from individual networks
+
+# Input
+folder.id = 'syn7264610' # Change the folder id argument for source networks
+
+# Load Libraries
+library(synapseClient)
+library(knitr)
+library(githubr)
+
+library(CovariateAnalysis)
+library(data.table)
+library(tidyr)
+library(plyr)
+library(dplyr)
+library(stringr)
+
+synapseLogin()
+
+source('./rankConsensus2.R')
+
+# Get source networks
+net.files = synQuery(paste0('select name,id from file where parentId == "', folder.id,'"')) 
+ind = setdiff(grep('.csv', net.files$file.name),
+              grep('rankConsensusNetwork', net.files$file.name))
+net.files = net.files[ind,]
+
+all.networks <- lapply(net.files$file.id, function(id){
+  net <- data.table::fread(synGet(id)@filePath, stringsAsFactors=FALSE, data.table=FALSE)
+  rownames(net) <- net$V1
+  net <- data.matrix(net[,-1])
+})
+gc()
+
+# Build rankconsensus
+rankConsensus <- rankConsensus2(all.networks)
+
+# Get github commit links
+thisRepo <- getRepo(repository = "th1vairam/metanetworkSynapse", 
+                    ref="branch", 
+                    refName='rank_cons')
+
+thisFile1 <- getPermlink(repository = thisRepo,
+                         repositoryPath = 'buildConsensus2.R')
+thisFile2 <- getPermlink(repository = thisRepo,
+                         repositoryPath = 'rankConsensus2.R')
+
+# Write results to synapse
+write.csv(rankConsensus, file = 'rankConsensusNetwork2.csv', quote=F, row.names = F)
+obj = File(file = 'rankConsensusNetwork2.csv', name = 'rankConsensusNetwork2.csv', parentId = folder.id)
+annotations(obj) = list(fileType = 'csv', resultsType = 'network', algorithm = 'rankconsensus')
+obj = synStore(obj, used = net.files$file.id, executed = c(thisFile1, thisFile2), activityName = 'Build rank consensus')
